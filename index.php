c59f36c267cefe35ef01534b8a977c8632b63802
Xinjian Qi <xinjianqi@yahoo.com>
2013-10-17 19:05:55 -0400

    added

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<title>KelioTech WebMaster Studio</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+	<div id="header">
+		<br>
+		<br>
+		<a id="headerContent">Hot</a>
+		<a id="headerContent">New</a>
+		<a id="headerContent">Sign in</a>
+		<a id="headerContent">Regiser</a>
+	</div>
+	<hr style="margin: 0px;">
+	<br>
+	<div id="body0">
+		<div id= "body1">hello1
+			
+		</div>
+		<div id="likeContent">
+			Like
+			<div>
+			Dislike
+			</div>
+		</div>
+		<div id="body2">
+			hello2
+		</div>
+	</div>
+	<br>
+	<div id="footer">
+		<br>
+		Last modified Monday, Octuber 16, 2013.
+		<br>&copy;
+		 KelioTech WebMaster Studio, New York.
+		<br>
+		Please contact Kenny with your questions, comments, and suggestions about this website.
+	</div>
+</body>
+</html>
