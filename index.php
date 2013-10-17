76fe41eeb1cbc6583001b278ad08781956e764a3
qxj1989 <xinjianqi@yahoo.com>
2013-10-16 22:58:40 -0400

    added index and style

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+	<div id="header">
+		<br>
+		<br>
+		<a id="headerContent">Hot</a>
+		<a id="headerContent">New</a>
+		<a id="headerContent">Sign in</a>
+		<a id="headerContent">Regiser</a>
+	</div>
+	<hr style="margin: 0px;">
+	<br>
+	<div id= "body1">
+		<div id="body2">
+			hello2
+		</div>
+	</div>
+	<br>
+	<div id="footer">
+		<br>
+		Last modified Monday, August 19, 2013.
+		<br>
+		© Ohlone College (510) 659-6000 Fremont and Newark, California.
+		<br>
+		Please contact the Web Team with your questions, comments, and suggestions about this website.
+	</div>
+</body>
+</html>
