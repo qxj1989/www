09bdd50f06f2a3543744534cbfd05504a0be20ed
qxj1989 <xinjianqi@yahoo.com>
2013-10-18 16:23:38 -0400

    did some work

--- index.php
+++ index.php
@@ -7,35 +7,41 @@
 	<div id="header">
 		<br>
 		<br>
-		<a id="headerContent">Hot</a>
-		<a id="headerContent">New</a>
-		<a id="headerContent">Sign in</a>
-		<a id="headerContent">Regiser</a>
+		<a id="headerContent1">BitAd</a>
+		<a id="headerContent1">Campaigns</a>
+		<a id="headerContent1">About</a>
+		<a id="headerContent1">Sentences</a>
 	</div>
-	<hr style="margin: 0px;">
+</div>
+<hr style="margin: 0px;">
+<br>
+<div id="body0">
+	<div id= "body1">
+	<img id="adFormat" src="ads/BitAd3.png" alt="ad1">
+	</div>
+	<div id="likeDislikeContent">
+		<div style="height 35%;">
+			<img src="images/like.png" alt="Like">
+		</div>
+		<div style="height 30%;">
+			<p style="color: white;">21,521</p>
+		</div>
+		<div style="height 35%;">
+			<img src="images/dislike.png" alt="Dislike">
+		</div>
+	</div>
+	<div id="body2">
+		Body2
+	</div>
+</div>
+<br>
+<div id="footer">
 	<br>
-	<div id="body0">
-		<div id= "body1">hello1
-			
-		</div>
-		<div id="likeContent">
-			Like
-			<div>
-				Dislike
-			</div>
-		</div>
-		<div id="body2">
-			hello2
-		</div>
-	</div>
+	Last modified Monday, Octuber 16, 2013.
+	<br>&copy;
+	KelioTech WebMaster Studio, New York.
 	<br>
-	<div id="footer">
-		<br>
-		Last modified Monday, Octuber 16, 2013.
-		<br>&copy;
-		KelioTech WebMaster Studio, New York.
-		<br>
-		Please contact Kenny with your questions, comments, and suggestions about this website.
-	</div>
+	Please contact Kenny with your questions, comments, and suggestions about this website.
+</div>
 </body>
 </html>
