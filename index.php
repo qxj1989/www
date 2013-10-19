f6622ed337640cac8034523217a17024ed49638f
qxj1989 <xinjianqi@yahoo.com>
2013-10-18 23:26:56 -0400

    did a lot of works

--- index.php
+++ index.php
@@ -12,36 +12,203 @@
 		<a id="headerContent1">About</a>
 		<a id="headerContent1">Sentences</a>
 	</div>
-</div>
-<hr style="margin: 0px;">
-<br>
-<div id="body0">
-	<div id= "body1">
-	<img id="adFormat" src="ads/BitAd3.png" alt="ad1">
-	</div>
-	<div id="likeDislikeContent">
-		<div style="height 35%;">
-			<img src="images/like.png" alt="Like">
-		</div>
-		<div style="height 30%;">
-			<p style="color: white;">21,521</p>
-		</div>
-		<div style="height 35%;">
-			<img src="images/dislike.png" alt="Dislike">
-		</div>
-	</div>
-	<div id="body2">
-		Body2
-	</div>
-</div>
-<br>
-<div id="footer">
+	<hr style="margin: 0px;">
 	<br>
-	Last modified Monday, Octuber 16, 2013.
-	<br>&copy;
-	KelioTech WebMaster Studio, New York.
+	<div id="body0">
+		<div id= "body1">
+			<img id="adFormat" src="ads/BitAd1.png" alt="ad1">
+			<div id="shareWraper">
+				<img id="shareIcon" src="icons/facebook.png" alt="fb">
+				<img id="shareIcon" src="icons/twitter.png" alt="tw">
+				<img id="shareIcon" src="icons/google+.png" alt="g+">
+				<img id="shareIcon" src="icons/mail.png" alt="mail">
+			</div>
+		</div>
+		<div id="likeDislikeContent">
+			<div style="height 35%;">
+				<img src="images/like.png" alt="Like">
+			</div>
+			<div style="height 30%;">
+				<p style="color: white;">21,521</p>
+			</div>
+			<div style="height 35%;">
+				<img src="images/dislike.png" alt="Dislike">
+			</div>
+		</div>
+		<div id="body2">
+			Body2
+		</div>
+		<!-- this is the body content -->
+		<div id="body1">
+			<img id="adFormat" src="ads/BitAd2.png" alt="ad1">
+			<div id="shareWraper">
+				<img id="shareIcon" src="icons/facebook.png" alt="fb">
+				<img id="shareIcon" src="icons/twitter.png" alt="tw">
+				<img id="shareIcon" src="icons/google+.png" alt="g+">
+				<img id="shareIcon" src="icons/mail.png" alt="mail">
+			</div>
+		</div>
+		<div id="likeDislikeContent">
+			<div style="height 35%;">
+				<img src="images/like.png" alt="Like">
+			</div>
+			<div style="height 30%;">
+				<p style="color: white;">21,521</p>
+			</div>
+			<div style="height 35%;">
+				<img src="images/dislike.png" alt="Dislike">
+			</div>
+		</div>
+		<div id="body1">
+			<img id="adFormat" src="ads/BitAd3.png" alt="ad1">
+			<div id="shareWraper">
+				<img id="shareIcon" src="icons/facebook.png" alt="fb">
+				<img id="shareIcon" src="icons/twitter.png" alt="tw">
+				<img id="shareIcon" src="icons/google+.png" alt="g+">
+				<img id="shareIcon" src="icons/mail.png" alt="mail">
+			</div>
+		</div>
+		<div id="likeDislikeContent">
+			<div style="height 35%;">
+				<img src="images/like.png" alt="Like">
+			</div>
+			<div style="height 30%;">
+				<p style="color: white;">21,521</p>
+			</div>
+			<div style="height 35%;">
+				<img src="images/dislike.png" alt="Dislike">
+			</div>
+		</div>
+		<div id="body1">
+			<img id="adFormat" src="ads/BitAd4.png" alt="ad1">
+			<div id="shareWraper">
+				<img id="shareIcon" src="icons/facebook.png" alt="fb">
+				<img id="shareIcon" src="icons/twitter.png" alt="tw">
+				<img id="shareIcon" src="icons/google+.png" alt="g+">
+				<img id="shareIcon" src="icons/mail.png" alt="mail">
+			</div>
+		</div>
+		<div id="likeDislikeContent">
+			<div style="height 35%;">
+				<img src="images/like.png" alt="Like">
+			</div>
+			<div style="height 30%;">
+				<p style="color: white;">21,521</p>
+			</div>
+			<div style="height 35%;">
+				<img src="images/dislike.png" alt="Dislike">
+			</div>
+		</div>
+		<div id="body1">
+			<img id="adFormat" src="ads/BitAd5.png" alt="ad1">
+			<div id="shareWraper">
+				<img id="shareIcon" src="icons/facebook.png" alt="fb">
+				<img id="shareIcon" src="icons/twitter.png" alt="tw">
+				<img id="shareIcon" src="icons/google+.png" alt="g+">
+				<img id="shareIcon" src="icons/mail.png" alt="mail">
+			</div>
+		</div>
+		<div id="likeDislikeContent">
+			<div style="height 35%;">
+				<img src="images/like.png" alt="Like">
+			</div>
+			<div style="height 30%;">
+				<p style="color: white;">21,521</p>
+			</div>
+			<div style="height 35%;">
+				<img src="images/dislike.png" alt="Dislike">
+			</div>
+		</div>
+		<div id="body1">
+			<img id="adFormat" src="ads/BitAd6.png" alt="ad1">
+			<div id="shareWraper">
+				<img id="shareIcon" src="icons/facebook.png" alt="fb">
+				<img id="shareIcon" src="icons/twitter.png" alt="tw">
+				<img id="shareIcon" src="icons/google+.png" alt="g+">
+				<img id="shareIcon" src="icons/mail.png" alt="mail">
+			</div>
+		</div>
+		<div id="likeDislikeContent">
+			<div style="height 35%;">
+				<img src="images/like.png" alt="Like">
+			</div>
+			<div style="height 30%;">
+				<p style="color: white;">21,521</p>
+			</div>
+			<div style="height 35%;">
+				<img src="images/dislike.png" alt="Dislike">
+			</div>
+		</div>
+		<div id="body1">
+			<img id="adFormat" src="ads/BitAd7.png" alt="ad1">
+			<div id="shareWraper">
+				<img id="shareIcon" src="icons/facebook.png" alt="fb">
+				<img id="shareIcon" src="icons/twitter.png" alt="tw">
+				<img id="shareIcon" src="icons/google+.png" alt="g+">
+				<img id="shareIcon" src="icons/mail.png" alt="mail">
+			</div>
+		</div>
+		<div id="likeDislikeContent">
+			<div style="height 35%;">
+				<img src="images/like.png" alt="Like">
+			</div>
+			<div style="height 30%;">
+				<p style="color: white;">21,521</p>
+			</div>
+			<div style="height 35%;">
+				<img src="images/dislike.png" alt="Dislike">
+			</div>
+		</div>
+		<div id="body1">
+			<img id="adFormat" src="ads/BitAd8.png" alt="ad1">
+			<div id="shareWraper">
+				<img id="shareIcon" src="icons/facebook.png" alt="fb">
+				<img id="shareIcon" src="icons/twitter.png" alt="tw">
+				<img id="shareIcon" src="icons/google+.png" alt="g+">
+				<img id="shareIcon" src="icons/mail.png" alt="mail">
+			</div>
+		</div>
+		<div id="likeDislikeContent">
+			<div style="height 35%;">
+				<img src="images/like.png" alt="Like">
+			</div>
+			<div style="height 30%;">
+				<p style="color: white;">21,521</p>
+			</div>
+			<div style="height 35%;">
+				<img src="images/dislike.png" alt="Dislike">
+			</div>
+		</div>
+		<div id="body1">
+			<img id="adFormat" src="ads/BitAd9.png" alt="ad1">
+			<div id="shareWraper">
+				<img id="shareIcon" src="icons/facebook.png" alt="fb">
+				<img id="shareIcon" src="icons/twitter.png" alt="tw">
+				<img id="shareIcon" src="icons/google+.png" alt="g+">
+				<img id="shareIcon" src="icons/mail.png" alt="mail">
+			</div>
+		</div>
+		<div id="likeDislikeContent">
+			<div style="height 35%;">
+				<img src="images/like.png" alt="Like">
+			</div>
+			<div style="height 30%;">
+				<p style="color: white;">21,521</p>
+			</div>
+			<div style="height 35%;">
+				<img src="images/dislike.png" alt="Dislike">
+			</div>
+		</div>
+		<!-- this is the body content -->
+	</div>
 	<br>
-	Please contact Kenny with your questions, comments, and suggestions about this website.
-</div>
+	<div id="footer">
+		<br>
+		Last modified Monday, Octuber 16, 2013.
+		<br>&copy;
+		KelioTech WebMaster Studio, New York.
+		<br>
+		Please contact Kenny with your questions, comments, and suggestions about this website.
+	</div>
 </body>
 </html>
